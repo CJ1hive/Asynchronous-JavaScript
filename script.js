1a976ebfc7713e5d8074f11f7564a8e4c40b5cf8
CJ1hive <112895301+CJ1hive@users.noreply.github.com>
2026-08-11 00:41:34 +0300

    Refactor country fetching logic to use renderCountry function and handle neighboring countries

--- script.js
+++ script.js
@@ -10,31 +10,108 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
-const getCountry = function (country) {
-  const request = new XMLHttpRequest();
+// const getCountry = function (coutnryName) {
+//   const request = new XMLHttpRequest(); // Oldschool way
+
+//   // XMLHttpRequest we need it in the future
+//   // Show us how old ajax used to function back in the day
+
+//   request.open(
+//     'GET',
+//     `https://restcountries.com/v2/name/${coutnryName.toLowerCase()}`
+//   );
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     const html = `
+//         <article class="country">
+//               <img class="country__img" src="${data.flag}" />
+//               <div class="country__data">
+//                 <h3 class="country__name">${data.name}</h3>
+//                 <h4 class="country__region">${data.region}</h4>
+//                 <p class="country__row"><span>👫</span>${(
+//                   +data.population / 1000000
+//                 ).toFixed(1)} M</p>
+//                 <p class="country__row"><span>🗣️</span>${
+//                   data.languages[0].name
+//                 }</p>
+//                 <p class="country__row"><span>💰</span>${
+//                   data.currencies[0].name
+//                 }</p>
+//               </div>
+//             </article>
+//         `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountry('Nigeria');
+// getCountry('Vietnam');
+// getCountry('Greece');
+// ✅✅✅✅✅✅
+
+// Callback Hell
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} M</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (coutnryName) {
+  const request = new XMLHttpRequest(); // Oldschool way
+
+  // XMLHttpRequest we need it in the future
+  // Show us how old ajax used to function back in the day
 
   request.open(
     'GET',
-    `restcountries.com/v3.1/name/${country}
-`,
+    `https://restcountries.com/v2/name/${coutnryName.toLowerCase()}`,
   );
   request.send();
+
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    const html = `
-  <article class="country">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>${data.currencies[0].name}</span>CUR</p>
-          </div>
-        </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+
+    renderCountry(data);
+
+    // Get AJAX CALL neighbor country (2) depends on call (1)
+    const neighbor = data.borders?.[0];
+    if (!neighbor) return;
+    const request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-getCountry(`iraq`);
+
+getCountryAndNeighbor('Argentina');
