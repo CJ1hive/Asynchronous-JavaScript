bbbb8a69121b9f1dc7e2302f539e71c02b2d0529
CJ1hive <112895301+CJ1hive@users.noreply.github.com>
2026-08-15 04:31:43 +0300

    Implement country rendering and error handling in getCountryData function

--- script.js
+++ script.js
@@ -3,6 +3,28 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -71,38 +93,27 @@ const countriesContainer = document.querySelector('.countries');
 // console.log(request);
 // getCountryData();
 
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flags.svg}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 const getCountryData = function (country) {
   fetch(`https://countries.dev/name/${country}`)
     .then(Response => Response.json())
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders[2];
-      console.log(neighbour);
+      console.log(data[0]);
+
+      const neighbour = data[0].borders[5];
       if (!neighbour) return;
 
       return fetch(`https://countries.dev/alpha/${neighbour}`);
     })
     .then(Response => Response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} there is an error`);
+      renderError(`something went wrong PAL ${err.message}. try again`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
   // .then(function (response) {
   //   console.log(response);
   //   return response.json();
@@ -111,4 +122,7 @@ const getCountryData = function (country) {
   //   console.log(data);
   // });
 };
-getCountryData(`iraq`);
+btn.addEventListener('click', function () {
+  getCountryData(`ukrain`);
+});
+getCountryData(`ajdsjadla`);
